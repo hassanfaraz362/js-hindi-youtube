addf3847865c4145f4da33ae92f9d6966167eeed
Hassan Faraz <hassanfaraas164@gmail.com>
2025-07-28 22:29:12 +0000

    Operation Added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,39 @@ let stringnumber = String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
 
+// *******************************OPERATIONS***************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " hassan"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) + 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
