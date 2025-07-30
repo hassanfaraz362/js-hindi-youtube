2e5462eac86c2707fae502508f52996334c282ff
Hassan Faraz <hassanfaraas164@gmail.com>
2025-07-30 20:49:42 +0000

    stack heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -51,3 +51,29 @@ const myFunction = function(){
 // Arrays => object
 // Function => function
 // Object => object
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap (Non-primitive)
+
+let myyoutubechannel = "hiteshkhan"
+
+let anothername = myyoutubechannel
+anothername = "chaiaurcode"
+
+console.log(myyoutubechannel);
+console.log(anothername);
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email = "malang@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
