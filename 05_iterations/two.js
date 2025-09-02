942a6bea93580de97ac5e8ba53487deea194c037
Hassan Faraz <hassanfaraas164@gmail.com>
2025-09-02 23:27:58 +0000

    2

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+    
+}
+
+let myArray = ['flash', 'batman', 'superhero']
+let arr = 0
+while (arr < myArray.length){
+    console.log(myArray[arr]);
+    arr = arr + 1
+}
+
+let score = 0
+
+do {
+    console.log(score)
+    score++
+    
+} while (score <= 10);
