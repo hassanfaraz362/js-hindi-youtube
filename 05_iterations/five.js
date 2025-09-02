8a78a95128b790aa0c8d5b5d5820d29fa7051fd6
Hassan Faraz <hassanfaraas164@gmail.com>
2025-09-02 23:33:04 +0000

    5

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+const coding =["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(  function (item) {
+    // console.log(item);
+    
+// })
+
+// coding.forEach(  (item) => {
+    // console.log(item);
+    
+// })
+
+// function printMe(item){
+    // console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach(  (item, index, arr)=> {
+    // console.log(item, index, arr);
+// })
+
+
+const mycoding = [
+    {
+        languagename: "jscript",
+        languagefilename: "js"
+    },
+    {
+        languagename: "java",
+        languagefilename: "java"
+    },
+    {
+        languagename: "python",
+        languagefilename: "py"
+    },
+]
+
+mycoding.forEach(  (item)=> {
+    
+    console.log(item.languagename);
+    
+})
