d1ab3d4d7419fbd98c80238ecfbcbcb228e9181e
Hassan Faraz <hassanfaraas164@gmail.com>
2025-09-02 23:30:10 +0000

    3

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+// for of loop
+
+//["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const grettings = "hello world"
+for (const greet of greetings) {
+    // console.log(greet);
+}
+
+//Maps
+
+const map = new Map
+map.set("IN", "India")
+map.set("USA","United states of America")
+map.set("Fr", "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    "game1" : "NFS",
+    "game2" : "spiderman"
+}
+
+// for (const [key, value] of myObject) {
+    // console.log(key, ':-', value);
+    
+    
+// }
