65d0de0c899e09a944066405f2b7d2073e4169bd
Hassan Faraz <hassanfaraas164@gmail.com>
2026-05-25 03:36:21 -0700

    Update oop.js

--- 10-classes-and-oop/oop.js
+++ 10-classes-and-oop/oop.js
@@ -5,7 +5,33 @@ const user ={
 
 getUserDetails: function(){
     // console.log("Get user details from database");
-    console.log(``)
+    console.log(`Username: ${this.username}`)
+    console.log(this)
 }
 
 }
+// console.log(user.username)
+console.log(user.getUserDetails)
+console.log(this);
+
+
+
+function User(username,loginCount, isloggedin){
+    this.username = username
+    this.loginCount = loginCount
+    this.isloggedin = isloggedin
+
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`);
+        
+    }
+
+    // return this
+}
+
+
+// when we use new keyword new empty object is created called instance
+const userone = new user('hitesh, 12, true') // 
+const usertwo = new user("chaiaurcode,11, false")
+console.log(userone.constructor);
+// console.log(usertwo);
