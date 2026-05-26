a6282d064f7236e1beab308b2d7b074afd3a1e3a
Hassan Faraz <hassanfaraas164@gmail.com>
2026-05-26 04:04:50 -0700

    Update prototype.js

--- 10-classes-and-oop/prototype.js
+++ 10-classes-and-oop/prototype.js
@@ -14,5 +14,52 @@ let heroPower= {
     }
 
 }
-Objes
-heroPower.hitesh()
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`)
+}
+
+Array.prototype.heyhistesh = function(){
+    console.log(`hitesh says hello`);
+    
+}
+// heroPower.hitesh()
+myheros.hitesh()
+myheros.heyhistesh()
+// heroPower.heyhistesh()
+
+// INHERTANCE
+const User = {
+    name : 'chai',
+    Email : 'chai@gmail.com'
+
+}
+
+const teacher= {
+    makevideo: true
+}
+
+const teachingSupport ={
+    isAvalible: false
+}
+
+const  TASupport = {
+    makeAssignment = 'jsAssignment',
+    fullTime: true,
+    __proto__:TeachingSupport
+}
+Teacher.__proto__ = User
+
+// modren syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherusername= 'chaiaurcode   '
+
+String.prototype.truelength = function(){
+    console.log(`${this}`);
+    console.log(`True length is ${this.trim().length}`);
+    
+}
+anotherusername.truelength()
+'hitesh'.truelength()
+'icetea'.truelength()
