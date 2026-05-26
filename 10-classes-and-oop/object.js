155962bbadda31a812ac68bbad9d0ea896ad0dbd
Hassan Faraz <hassanfaraas164@gmail.com>
2026-05-26 11:02:42 +0000

    prototypic inheritance

---
+++ 10-classes-and-oop/object.js
@@ -0,0 +1,31 @@
+function multiplyby5(num) {
+    return num*5
+}
+
+multiplyby5.power = 2
+
+console.log(multiplyby5(5));
+console.log(multiplyby5.power)
+console.log(multiplyby5.prototype);
+
+function createuser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createuser.prototype.increment = function(){
+    this.score++
+}
+
+createuser.prototype.printMe= function(){
+    console.log(`price is ${this.score}`);
+    
+}
+const chai =new createuser('chai', 25)
+const tea =new createuser('tea', 250)
+
+chai.printMe()
+
+
+
+
---
+++ 10-classes-and-oop/prototype.js
@@ -0,0 +1,18 @@
+// let myname = 'hitesh     '
+// let mychannel = 'chai  '
+
+// console.log(myname.truelength);
+
+let myheros= ['thor', 'spiderman']
+
+let heroPower= {
+    thor: 'hammer',
+    spiderman: 'sling',
+
+    getSpiderPower: function(){
+        console.log(`spidy power is ${this.spiderman}`)
+    }
+
+}
+Objes
+heroPower.hitesh()
