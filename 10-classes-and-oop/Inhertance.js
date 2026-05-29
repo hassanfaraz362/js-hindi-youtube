3216b31de8950a4e3d92f8a352eff255e4b86d5a
Hassan Faraz <hassanfaraas164@gmail.com>
2026-05-29 21:13:01 +0000

    class constructor

---
+++ 10-classes-and-oop/Inhertance.js
@@ -0,0 +1,30 @@
+class user{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USENAME is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends user{
+    constructor(username, email, password){
+        super(username)
+        this.email=email
+        this.password=password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+const chai = new Teacher ('chai', 'chai@teacher.com', '123')
+chai.addCourse()
+
+const masalachai = new user('masalachai')
+masalachai.logMe()
+
+console.log(chai instanceof user);
